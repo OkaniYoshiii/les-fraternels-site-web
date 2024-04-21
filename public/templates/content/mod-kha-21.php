ff5c7aa4718d988c4710304a1ae01d5c418787d5
OkaniYoshiii <dragut31620@gmail.com>
2024-04-21 22:22:08 +0200

    Ajout de plusieurs mods (partiellement)

---
+++ public/templates/content/mod-kha-21.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php require_once HTML_HEAD_DIR . '/metadata.php'; ?>
+    <?php require_once HTML_HEAD_DIR . '/default-scripts.php'; ?>
+    <?php require_once HTML_HEAD_DIR . '/favicon.php'; ?>
+    <title>Mods - <?= $mod->getName(); ?></title>
+    <meta name="description" content="<?= $mod->getDescription() ?>">
+    <meta name="keywords" content="7 Days To Die, 7DTD, FRT, Mods, Mod, HUD, UI">
+    <link rel="stylesheet" href="<?= SITE_URL . CSS_DIR ?>/default-mod.css">
+</head>
+
+<body class="no-js">
+    <?php require_once HTML_DIR . "/header.php"; ?>
+    <main>
+        <?php require_once TEMPLATE_PARTS_DIR . '/single-mod-banner.php'; ?>
+        <section class="narrow">
+            <h2 id="description">Description</h2>
+            <p><?= $mod->getDescription() ?></p>
+        </section>
+    </main>
+    <?php require_once HTML_DIR . "/footer.php"; ?>
+</body>
+</html>
