47733493bba42ff2412433e7a5226c3743635516
OkaniYoshiii <dragut31620@gmail.com>
2024-04-30 19:55:46 +0200

    Tentative de relation entre git local et site web

--- db_config.php
+++ db_config.php
@@ -2,6 +2,6 @@
 
 // Database credentials (DEV)
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'mods_db');
-define('DB_USER', 'root');
-define('DB_PWD', '');
+define('DB_NAME', 'u547807080_mods_db');
+define('DB_USER', 'u547807080_I78AyjIn');
+define('DB_PWD', '*yTmaY+v^U5');
