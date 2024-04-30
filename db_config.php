d66b96f2aebcdc9097d1707337cabaaa08fd6071
OkaniYoshiii <dragut31620@gmail.com>
2024-04-30 20:30:48 +0200

    Suppression de db_config

--- db_config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-// Database credentials (DEV)
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'u547807080_mods_db');
-define('DB_USER', 'u547807080_I78AyjIn');
-define('DB_PWD', '*yTmaY+v^U5');
