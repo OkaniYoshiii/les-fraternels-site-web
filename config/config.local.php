ef4fedd7967e37c5c15cbe3cfc72a29ba60da048
OkaniYoshiii <dragut31620@gmail.com>
2024-06-01 09:50:21 +0200

    Ajout du fichier 'config.local.php' dans le gitignore + ajout d'un template vide pour ce même fichier

--- config/config.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('SITE_URL', 'http://localhost/public/');
-
-// Database credentials
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'mods_db');
-define('DB_USER', 'root');
-define('DB_PWD', '');
---
+++ config/config.local.template.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+    MODELE POUR LE FICHIER 'config.local.php' :
+
+    Permet de renseigner les informations liées à l'environnement sur lequel pvous vous trouvez. 
+    Exemple : informations sur la base de données, nom de domaine etc ...
+
+    Ce fichier étant un template, il vous faudra remplir les informations mentionnées ci-dessus puis
+    renommer le fichier en 'config.local.php' car c'est ce fichier là qui sera ensuite utilisé par l'application
+*/
+
+// Nom de domaine avec slash de fin (ex: http://localhost/public/ ou  https://wikipedia.org/)
+define('SITE_URL', 'http://localhost/public/');
+
+// Database credentials
+define('DB_HOST', 'localhost');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PWD', '');
